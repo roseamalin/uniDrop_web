b306426825df9f2ebc5028e439cf060dec22d832
roseamalin <roseamalinrose@gmail.com>
2025-07-05 18:35:06 +0800

    Update LoginBeanUni.java

--- src/java/beanUni/LoginBeanUni.java
+++ src/java/beanUni/LoginBeanUni.java
@@ -1,26 +1,73 @@
+//ADMIN
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package beanUni;
+package bean;
 
 /**
  *
  * @author najlaa
  */
-public class LoginBeanUni implements java.io.Serializable {
+public class AdminLoginBean implements java.io.Serializable {
     
     public String username;
     public String password;
 
-    public LoginBeanUni(String username, String password) {
+    public AdminLoginBean(String username, String password) {
         this.username = username;
         this.password = password;
     }
     
 
-    public LoginBeanUni() {
+    public AdminLoginBean() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
+
+
+
+//CUSTOMER
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author najlaa
+ */
+public class CustLoginBean implements java.io.Serializable {
+    
+    public String username;
+    public String password;
+
+    public CustLoginBean(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+
+    public CustLoginBean() {
     }
 
     public String getUsername() {
