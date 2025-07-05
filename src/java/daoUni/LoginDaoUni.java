4972778e339ae7fab2b31c6c8509d814c9e0d7cc
roseamalin <roseamalinrose@gmail.com>
2025-07-05 18:36:29 +0800

    Update LoginDaoUni.java

--- src/java/daoUni/LoginDaoUni.java
+++ src/java/daoUni/LoginDaoUni.java
@@ -1,21 +1,22 @@
+//ADMIN
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package daoUni;
+package dao;
 
-import beanUni.LoginBeanUni;
+import bean.AdminLoginBean;
 import java.sql.*;
-import utilUni.DBConnectionUni;
+import util.DBConnection;
 
 /**
  *
  * @author najlaa
  */
-public class LoginDaoUni {
+public class AdminLoginDao {
     
-    public String authenticateUser(LoginBeanUni loginBean) {
+    public String authenticateUser(AdminLoginBean loginBean) {
         
         String username = loginBean.getUsername();
         String password = loginBean.getPassword();
@@ -27,9 +28,58 @@ public class LoginDaoUni {
         String passwordDB = "";
         
         try {
-            con = DBConnectionUni.createConnection();
+            con = DBConnection.createConnection();
             statement = con.createStatement();
-            resultSet = statement.executeQuery("select * from campusClubDB");
+            resultSet = statement.executeQuery("select * from admin");
+            
+            while (resultSet.next())
+            {
+                usernameDB = resultSet.getString("username");
+                passwordDB = resultSet.getString("password");
+                if (username.equals(usernameDB) && password.equals(passwordDB)) {
+                    return "SUCCESS";
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Invalid user credentials";
+    }
+}
+
+//CUSTOMER
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.CustLoginBean;
+import java.sql.*;
+import util.DBConnection;
+
+/**
+ *
+ * @author najlaa
+ */
+public class CustLoginDao {
+    
+    public String authenticateUser(CustLoginBean loginBean) {
+        
+        String username = loginBean.getUsername();
+        String password = loginBean.getPassword();
+        
+        Connection con = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String usernameDB = "";
+        String passwordDB = "";
+        
+        try {
+            con = DBConnection.createConnection();
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("select * from customers");
             
             while (resultSet.next())
             {
