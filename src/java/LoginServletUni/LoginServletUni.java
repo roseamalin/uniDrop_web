da96444593512d2e2c51070c1095c1e66eb2ffa0
roseamalin <roseamalinrose@gmail.com>
2025-07-05 18:32:55 +0800

    Update LoginServletUni.java
    NAMA PACKAGE LoginServletUni SAMA GAK NGN LOGOUT TAMBAH UNI TAU
    ATAS ADMIN BAWAH CUSTOMER

--- src/java/LoginServletUni/LoginServletUni.java
+++ src/java/LoginServletUni/LoginServletUni.java
@@ -1,3 +1,4 @@
+//ADMIN
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -112,3 +113,120 @@ public class LoginServletUni extends HttpServlet {
     }// </editor-fold>
 
 }
+
+//CUSTOMER
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LoginServletUni;
+
+import bean.CustLoginBean;
+import dao.CustLoginDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author najlaa
+ */
+public class CustLoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+         String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        CustLoginBean loginBean = new CustLoginBean();
+        
+        loginBean.setUsername(username);
+        loginBean.setPassword(password);
+        
+        CustLoginDao loginDao = new CustLoginDao();
+        
+    
+        
+        String userValidate = loginDao.authenticateUser(loginBean);
+    
+        if(userValidate.equals("SUCCESS"))
+        {
+            request.setAttribute("username", username);
+            request.getRequestDispatcher("/user_panel.jsp").forward(request, response);
+        }
+        else
+        {
+            request.setAttribute("errMessage", userValidate);
+            request.getRequestDispatcher("/customer_login.jsp").forward(request, response);
+        }
+    }
+    
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
